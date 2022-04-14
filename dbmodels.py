85c1f01040bc14c2f2ba71380bacbf067913d790
cryptondr <83161554+cryptondr@users.noreply.github.com>
2022-04-14 18:26:51 +0200

    updating database columns and tables

--- dbmodels.py
+++ dbmodels.py
@@ -1,34 +1,14 @@
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-import json
-from sqlalchemy.ext import mutable
 from flask import Flask
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "saj21#12da!s321@das*(aas$as6"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nutridatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-
-class JsonEncodedDict(db.TypeDecorator):
-    """Enables JSON storage by encoding and decoding on the fly."""
-    impl = db.Text
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return '{}'
-        else:
-            return json.dumps(value)
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return {}
-        else:
-            return json.loads(value)
-
-
-mutable.MutableDict.associate_with(JsonEncodedDict)
 
 
 class User(UserMixin, db.Model):
@@ -40,17 +20,23 @@ class User(UserMixin, db.Model):
     gender = db.Column(db.String(10), nullable=False)
     height = db.Column(db.Integer, nullable=False)
     weight = db.Column(db.Integer, nullable=False)
-    calorie_eaten = db.Column(JsonEncodedDict)
-    eaten_details = db.Column(JsonEncodedDict)
-    calorie_burnt = db.Column(JsonEncodedDict)
-    exercise_details = db.Column(JsonEncodedDict)
+    exercises = db.relationship('Exercise', backref="user")
 
 
+class Exercise(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(300))
+    duration = db.Column(db.Integer)
+    calories_burnt = db.Column(db.Integer)
+    date = db.Column(db.String(10))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+db.create_all()
 
 def create_tables():
     db.create_all()
 
 
-def adduser(user):
-    db.session.add(user)
+def add_to_datebase(value):
+    db.session.add(value)
     db.session.commit()
